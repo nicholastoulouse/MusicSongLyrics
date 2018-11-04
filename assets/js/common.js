3ac1996de57b5679416feb97b3bf0aba78640663
nicoto <toulousenicholas@gmail.com>
2018-11-03 22:13:13 -0700

    api key code fixed

---
+++ assets/js/common.js
@@ -0,0 +1,8 @@
+
+// Be sure to ope the index.html file locally before trying to open https://nicholastoulouse.github.io/MusicSongLyrics/
+// Or the API keys won't be loaded in your browser first.
+
+Storage.prototype.getObject = function(key) {
+    var value = this.getItem(key);
+    return value && JSON.parse(value);
+}
